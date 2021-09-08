98ec6cb2a50f3a086e02e4486b53f86d81fdfefd
Adriano <adriano.porto.a.j@gmail.com>
2021-09-08 20:58:37 -0300

    Setting up a server w Express

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { resolve, join } from 'path'
+
+const viewsPath = resolve('public', 'views')
+
+const app = express()
+
+app
+    .use(express.static('public'))
+    .get('/', (req, res) => {
+        return res.sendFile(join(viewsPath, 'index.html'))
+    })
+    .listen(3333, () => {console.log("LISTENING ON 3333")})
